45c7e4d8ed236e8f44a3ebb99f232465bd0e344c
Manny <wewen@Mannys-iMac-Pro.local>
2021-01-16 21:41:58 -0500

    did 191 poggers

---
+++ solutions/191_number_of_1_bits/191.py
@@ -0,0 +1,36 @@
+#Write a function that takes an unsigned integer and returns the number of '1'
+#bits it has (also known as the Hamming weight).
+#
+#Note:
+#
+#Note that in some languages such as Java, there is no unsigned integer type. In
+#this case, the input will be given as a signed integer type. It should not
+#affect your implementation, as the integer's internal binary representation is
+#the same, whether it is signed or unsigned.  In Java, the compiler represents
+#the signed integers using 2's complement notation. Therefore, in Example 3
+#above, the input represents the signed integer. -3.  Follow up: If this
+#function is called many times, how would you optimize it?
+#
+# 
+#
+#Example 1:
+#
+#Input: n = 00000000000000000000000000001011
+#Output: 3
+#Explanation: The input binary string 00000000000000000000000000001011 has a total of three '1' bits.
+n = 11
+ans = 3
+class Solution:
+    def hammingWeight(self, n):
+        total = 0
+        
+        while n != 0:
+            if n % 2 == 1:
+                total += 1
+            n = int(n/2)
+        
+        
+        return total
+
+print(f"Input: {n}, Correct Answer: {ans}")
+print(f"My answer: {Solution().hammingWeight(n)}")
